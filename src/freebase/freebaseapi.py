67b77e6d0059c10e4bc1b1dbc266a43b5b19fc3d
johnmcconnell <johnnyillinois@gmail.com>
2014-07-03 21:06:42 -0500

    Added freebase api -> src/freebase/FreebaseAPI

--- src/freebase/freebaseapi.py
+++ src/freebase/freebaseapi.py
@@ -1,1 +1,13 @@
-###TO DO FREEBASE###
+import json
+
+import urllib2
+import urllib
+
+api_key='AIzaSyD8dW6J4JSy2oZw5kPgP69-JCXNMKHlqjE'
+def query(string):
+	encoded_parameters = urllib.urlencode( \
+			{'query': query, 'key' : api_key} \
+			)
+	request = urllib2.urlopen('https://www.googleapis.com/freebase/v1/search?' + \
+			encoded_parameters)
+	return json.loads(request.read())

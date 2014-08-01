06b92215a25981ad8c84a9a85b4cbd37706359b7
ttjiang2 <ttjiang2@illinois.edu>
2014-07-31 19:31:32 -0500

    remove old reddit api connector

--- src/reddit/RedditAPI.py
+++
@@ -1,9 +0,0 @@
-import json
-
-import urllib
-import urllib
-
-api_urls = {'new':'www.reddit.com/new.json'}
-def get_new_posts():
-	request = urllib2.urlopen(api_urls['new'])
-	return json.loads(request.read())
